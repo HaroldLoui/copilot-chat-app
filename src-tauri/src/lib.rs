feafa34aac2370ce72d8cd752a9c79253d37535e
HaroldLoui <2015102239@nit.edu.cn>
2024-11-24 21:42:21 +0800

    新增sqlite

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use rusqlite::Connection;
+use tauri::{App, Manager};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,8 +10,57 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(setup)
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
+fn setup(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
+    // app.manage(state)
+    let app_path = app.path();
+    let resource_path = app_path.resource_dir()?;
+    println!("{:?}", resource_path);
+    let db_path = resource_path.join("data.db");
+    let conn = Connection::open(db_path)?;
+    // conn.execute(
+    //     "CREATE TABLE person (
+    //         id    INTEGER PRIMARY KEY,
+    //         name  TEXT NOT NULL,
+    //         data  BLOB
+    //     )",
+    //     (), // empty list of parameters.
+    // )?;
+    let me = Person {
+        id: 0,
+        name: "Steven".to_string(),
+        data: None,
+    };
+    conn.execute(
+        "INSERT INTO person (name, data) VALUES (?1, ?2)",
+        (&me.name, &me.data),
+    )?;
+
+    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
+    let person_iter = stmt.query_map([], |row| {
+        Ok(Person {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            data: row.get(2)?,
+        })
+    })?;
+
+    for person in person_iter {
+        println!("Found person {:?}", person.unwrap());
+    }
+    Ok(())
 }
